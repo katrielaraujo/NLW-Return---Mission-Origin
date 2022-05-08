45e241bdc3c6c2c35c822e931d5d24a554b5e441
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2022-05-07 23:46:29 -0300

    aula de quarta encerrada com sucesso e terminando aula de quinta

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu-expanded');
 }
+
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 400,
+}).reveal(`
+    #home,
+    #home img,
+    #home .stats,
+    #services,
+    #services header,
+    #services .cards,
+    #about,
+    #about header,
+    #about .content`);
