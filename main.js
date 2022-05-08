f8d8dea5d17a2120e8c19b4e539b02b603f4d638
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2022-05-08 01:19:57 -0300

    terminada aula de quinta colocando cores livremente

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-function onScroll(){
+window.addEventListener('scroll',onScroll);
 
+onScroll();
+function onScroll(){
+    showNavOnScroll();
+    backToTopButtonOnScroll();
+}
+
+function showNavOnScroll(){
     if(scrollY > 0){
-        navigation.classList.add("scroll");
+        navigation.classList.add('scroll');
     }else{
-        navigation.classList.remove("scroll");
+        navigation.classList.remove('scroll');
+    }
+}
+
+function backToTopButtonOnScroll(){
+    if(scrollY > 700){
+        backToTopButton.classList.add('show');
+    }else{
+        backToTopButton.classList.remove('show');
     }
 }
 
