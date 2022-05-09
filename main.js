f6f99adc9569953b3a8d60b6cbf47a3780913d7c
Katriel Albuquerque Galvão de Araújo <katrielaraujo@gmail.com>
2022-05-08 23:34:54 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -4,7 +4,34 @@ onScroll();
 function onScroll(){
     showNavOnScroll();
     backToTopButtonOnScroll();
+    activeMenuAtCurrentSection(home);
+    activeMenuAtCurrentSection(services);
+    activeMenuAtCurrentSection(about);
+    activeMenuAtCurrentSection(contact);
 }
+
+function activeMenuAtCurrentSection(section){
+    //linha alvo
+    const targetLine = scrollY + innerHeight / 2;
+
+    //to topo da seção
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
+
+    const sectionEndsAt = sectionTop + sectionHeight;
+    const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+    const sectionId = section.getAttribute('id');
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active');
+    if(sectionBoundaries){
+        menuElement.classList.add('active');
+    }
+} 
 
 function showNavOnScroll(){
     if(scrollY > 0){
@@ -44,3 +71,7 @@ ScrollReveal({
     #about,
     #about header,
     #about .content`);
+
+function sayMyName(name){
+    console.log(name)
+}
